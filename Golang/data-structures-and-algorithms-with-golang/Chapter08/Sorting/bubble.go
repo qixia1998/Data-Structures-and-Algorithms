e0599ae6da3ff16906b603e7b05cefb89ad64825
qixia1998 <qixia717138552@gmail.com>
2023-09-18 00:44:43 +0800

    add bubble

---
+++ Golang/data-structures-and-algorithms-with-golang/Chapter08/Sorting/bubble.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func bubbleSorter(integers [11]int) {
+	var num int
+	num = 11
+	var isSwapped bool
+	isSwapped = true
+	for isSwapped {
+		isSwapped = false
+		var i int
+		for i = 1; i < num; i++ {
+			if integers[i-1] > integers[i] {
+				var temp = integers[i]
+				integers[i] = integers[i-1]
+				integers[i-1] = temp
+				isSwapped = true
+			}
+		}
+	}
+	fmt.Println(integers)
+}
+
+func main() {
+	var integers [11]int = [11]int{31, 13, 12, 4, 18, 16, 7, 2, 3, 0, 10}
+	fmt.Println("Bubble Sorter")
+	bubbleSorter(integers)
+}
