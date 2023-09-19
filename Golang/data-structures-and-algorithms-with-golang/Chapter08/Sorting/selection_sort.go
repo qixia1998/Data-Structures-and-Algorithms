5d5abf5d8d5a4f8624a34c8dc53edadb7f42b173
qixia1998 <qixia717138552@gmail.com>
2023-09-19 23:53:19 +0800

    add sort

---
+++ Golang/data-structures-and-algorithms-with-golang/Chapter08/Sorting/selection_sort.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func SelectionSorter(elements []int) {
+	var i int
+	for i = 0; i < len(elements)-1; i++ {
+		var min int
+		min = i
+		var j int
+		for j = i + 1; j <= len(elements)-1; j++ {
+			if elements[j] < elements[min] {
+				min = j
+			}
+		}
+		swap(elements, i, min)
+	}
+}
+
+func swap(elements []int, i int, j int) {
+	var temp int
+	temp = elements[j]
+	elements[j] = elements[i]
+	elements[i] = temp
+}
+
+func main() {
+	var elements []int
+	elements = []int{11, 4, 18, 6, 19, 21, 71, 13, 15, 2}
+	fmt.Println("Before Sorting ", elements)
+	SelectionSorter(elements)
+	fmt.Println("After Sorting", elements)
+}
