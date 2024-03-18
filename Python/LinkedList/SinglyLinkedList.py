8086ddbbfa4f7533c6d539384dadd4c398362d14
qixia1998 <qixia717138552@gmail.com>
2024-03-18 22:43:44 +0800

    update singly linkedlist

---
+++ Python/LinkedList/SinglyLinkedList.py
@@ -0,0 +1,30 @@
+# 单链表
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+    
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def append(self, data):
+        node = Node(data)
+        if self.tail:
+            self.tail.next = node
+            self.tail = node
+        else:
+            self.head = node
+            self.tail = node
+
+words = SinglyLinkedList()
+words.append('egg')
+words.append('ham')
+words.append('spam')
+
+current = words.head
+while current:
+    print(current.data)
+    current = current.next
